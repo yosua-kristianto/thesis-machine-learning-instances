da485f44998f9eb3b5333bef0af00d3f2bf26fc3
Yosua Kristianto <yosua_kristianto144@outlook.com>
2024-02-29 20:24:22 +0700

    Added DocBankHelper functionality

--- Upload Controller/Handler/CommandHandler.fs
+++ Upload Controller/Handler/CommandHandler.fs
@@ -1,6 +1,12 @@
 ﻿namespace Handler
 
+open System;
+open System.IO;
+
 open Model.Entity;
+open Facade.EnvironmentVariable;
+open Facade.ProgressBarHandler;
+open Repository;
 
 (*
     Define Command Pattern
@@ -13,9 +19,9 @@ open Model.Entity;
 
     | Splits | Ratio |
     |---|---|
-    | Train | 8 |
-    | Test | 1 |
-    | Val | 1 |
+    | Train | 7 |
+    | Test | 1.5 |
+    | Val | 1.5 |
 
     - The data to be uploaded are determined by LowRes version of the file. Basically you take the file name, search for any FOLDER CODE of SRGAN*, with file name of it.
 
@@ -27,12 +33,67 @@ open Model.Entity;
 *)
 module CommandHandler =
 
-    let CommandParser (folderCode: string) (dataframe: DataFrame) =
+    (*
+        TranslateFolderCodeToLocalPath
 
-        if dataframe.FOLDER_CODE.Contains("SRGAN") then
-            1+1
-        elif dataframe.FOLDER_CODE.Contains("YOLO") then
-            2+2
-        else
-            3+3
+        This function used to translate the folder code, with the original path for the operation to be done.
+
+        @param folderCode
+        The destinated folder code
+
+        @return string
+    *)
+    let TranslateFolderCodeToLocalPath(folderCode: string): string =
+        match folderCode with
+                            | "SRGAN_LOWRES" -> EnvironmentVariable.DOWNSCALED_UPSCALED_IMAGE_FOLDER_PATH
+                            | "YOLO_TRAIN_IMAGE" -> EnvironmentVariable.YOLO_IMAGE_FOLDER + "/Train"
+                            | "YOLO_TEST_IMAGE" -> EnvironmentVariable.YOLO_IMAGE_FOLDER + "/Test"
+                            | "YOLO_VAL_IMAGE" -> EnvironmentVariable.YOLO_IMAGE_FOLDER + "/Val"
+                            | "YOLO_TRAIN_LABEL" -> EnvironmentVariable.YOLO_ANNOTATION_FOLDER_OUTPUT + "/Train"
+                            | "YOLO_TEST_LABEL" -> EnvironmentVariable.YOLO_ANNOTATION_FOLDER_OUTPUT + "/Test"
+                            | "YOLO_VAL_LABEL" -> EnvironmentVariable.YOLO_ANNOTATION_FOLDER_OUTPUT + "/Val"
+
+    
+    (*
+        CommandParser
+
+        This function helps on controlling all uploader handlers.
+
+        This function will skip SRGAN_HIGHRES and SRGAN_LABEL functionality since the DocBankHandler
+        will handle the file directly by its file name.
+    *)
+    let CommandParser (dataframe: DataFrame) (repository: IRegisteredFileRepository) =
+
+        let mutable isSRGANProcessed: bool = false;
+
+        if dataframe.FOLDER_CODE.Contains "SRGAN" && dataframe.FOLDER_CODE <> "SRGAN_LOWRES" then
+            //printfn "%s is skipped since it will be controlled directly from the LOWRES version." dataframe.FOLDER_CODE;
+            ()
+
+        // Init The DocBankHandler and do the operation
+        if dataframe.FOLDER_CODE.Equals("SRGAN_LOWRES") then
+            let handler = new DocBankHandler(repository);
+
+            // Get files from lowres folder
+            let lowresFiles = Directory.GetFiles(TranslateFolderCodeToLocalPath dataframe.FOLDER_CODE);
+
+            // Count total files within the folder
+            handler.AllCount <- lowresFiles.Length;
+
+            // Train test split
+            // The test using "ceil" instead of round or floor, to round up in case count - train / 2 getting 1 modulus.
+            handler.TrainTreshold <- int(round(float(handler.AllCount) * 0.7));
+            handler.TestTreshold <- int(ceil(float((handler.AllCount - handler.TrainTreshold)) / 2.0));
+            handler.ValTreshold <- handler.AllCount - handler.TrainTreshold - handler.TestTreshold
+
+            let mutable iteration: int = 0;
+            let disposable, updateProgress = tqdm handler.AllCount "SRGAN Progressor"
+
+            printfn "Processing all SRGAN related files.";
+            for e in lowresFiles do
+                handler.ProcessAllFiles(Path.GetFullPath(e));
+                iteration <- iteration + 1;
+                updateProgress iteration;
+            disposable.Dispose();
+                
 
