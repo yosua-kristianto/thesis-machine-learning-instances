da85de0f5df5c69185c41e2eace1fd11c78a4eca
Yosua Kristianto <yosua_kristianto144@outlook.com>
2024-02-29 20:22:51 +0700

    Added Thread Name for the progressor

---
+++ Upload Controller/Facade/ProgressBarHandler.fs
@@ -0,0 +1,19 @@
+﻿namespace Facade
+
+open System.Threading;
+
+module ProgressBarHandler =
+
+    let tqdm (total : int) (threadName: string) =
+        let updateProgress (current : int) =
+            let progress = float current / float total * 100.
+            printf "\rProgress [%s]: [%-20s] %.2f%%" threadName (String.replicate (current * 20 / total) "#") progress
+            System.Console.Out.Flush() |> ignore
+
+        let dispose() =
+            printfn ""
+
+        { new System.IDisposable with
+            member this.Dispose() = dispose() }, updateProgress
+
+    
