bf95aa9a8c089a824966663014a2f4ca10e29efd
Yosua Kristianto <yosua_kristianto144@outlook.com>
2024-03-27 15:32:29 +0700

    Change call of HandleDownscaleUpscaleImage to HandleSuperResolutionDataset

--- Pre-Processing/sr-preprocessor/Main.fs
+++ Pre-Processing/sr-preprocessor/Main.fs
@@ -4,9 +4,22 @@ open System.IO;
 open Model.Dto;
 open Facade.EnvironmentVariable;
 open Facade;
+open Facade.ProgressBarHandler;
 open Services;
 open Handler.ImageProcessorHandler;
 
+
+printfn "
+ (   (      (                (                                   
+ )\ ))\ )   )\ )             )\ )                                
+(()/(()/(  (()/((     (     (()/((              (           (    
+ /(_))(_))  /(_))(   ))\ ___ /(_))(   (    (   ))\(  (   (  )(   
+(_))(_))   (_))(()\ /((_)___(_))(()\  )\   )\ /((_)\ )\  )\(()\  
+/ __| _ \  | _ \((_|_))     | _ \((_)((_) ((_|_))((_|(_)((_)((_) 
+\__ \   /  |  _/ '_/ -_)    |  _/ '_/ _ \/ _|/ -_|_-<_-< _ \ '_| 
+|___/_|_\  |_| |_| \___|    |_| |_| \___/\__|\___/__/__|___/_|  
+\n\n
+"
 
 // Utils
 open Microsoft.FSharp.Core.Operators;
@@ -17,15 +30,19 @@ let files = Directory.GetFiles(EnvironmentVariable.ORIGINAL_IMAGE_DIRECTORY);
 TelegramService.SendMessage "Starting the upscale-downscale image generation operation";
 Log.I "Starting the upscale-downscale image generation operation" |> ignore;
 
+let disposable, updateProgress = tqdm files.Length "SRGAN Dataset Pre-Processing";
+let mutable iteration: int = 0;
     
 for i in files do
-    printfn "Processing %s" i 
-
     try
-        HandleDownscaleUpscaleImage i |> ignore;
+        HandleSuperResolutionDataset i |> ignore;
     with 
     | ex ->
         Log.E ("Processing " + i + " error with: " + ex.Message) |> ignore;
+
+    iteration <- iteration + 1;
+    updateProgress iteration;
+disposable.Dispose();
 
 TelegramService.SendMessage "The upscale-downscale image generation operation has been completed";
 Log.I "The upscale-downscale image generation operation has been completed" |> ignore;
